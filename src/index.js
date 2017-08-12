3554f1889433ed27cb303ca0e34536a47f23ace2
Guanghui Wang <wghglory89@gmail.com>
2017-08-12 20:09:05 +0800

    refactoring store, entry root

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './store';
-import App from './components/App';
+import Root from './components/Root';
+import configureStore from './store/index';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+const store = configureStore();
+ReactDOM.render(<Root store={store} />, document.getElementById('root'));
--- src/store/index.js
+++ src/store/index.js
@@ -6,34 +6,28 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { loadState, saveState } from './localStorage';
 import throttle from 'lodash/throttle';
 
-// not recommended since it would become more difficult to test and change your reducers later.
-const persistedState = {
-  todos: [
-    {
-      id: 0,
-      text: 'Welcome Back!',
-      completed: false
-    }
-  ]
+const configureStore = () => {
+  const persistedState = loadState();
+  const store = createStore(
+    reducer,
+    persistedState,
+    composeWithDevTools(
+      applyMiddleware() //pass ...middleware
+      // other store enhancers if any
+    )
+  );
+
+  // throttle will make sure saveState won't be called more than 1 second. we don't want this expensive function executes too frequently
+  store.subscribe(
+    throttle(() => {
+      saveState({
+        todos: store.getState().todos
+      });
+    }, 1000)
+  );
+
+  return store;
 };
 
-const store = createStore(
-  reducer,
-  // persistedState,
-  loadState(),
-  composeWithDevTools(
-    applyMiddleware() //pass ...middleware
-    // other store enhancers if any
-  )
-);
-
-// throttle will make sure saveState won't be called more than 1 second. we don't want this expensive function executes too frequently
-store.subscribe(
-  throttle(() => {
-    saveState({
-      todos: store.getState().todos
-    });
-  }, 1000)
-);
-
-export default store;
+// By exporting configureStore instead of just store, we will be able to create as many store instances as we want for testing.
+export default configureStore;
