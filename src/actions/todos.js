33ad7a658142b2caaec134304f6d09c060200c2c
Guanghui Wang <wghglory89@gmail.com>
2017-08-22 00:00:19 +0800

    27 toggle todo on server

--- src/actions/todos.js
+++ src/actions/todos.js
@@ -14,10 +14,13 @@ export const addTodo = text => dispatch =>
     });
   });
 
-export const toggleTodo = id => ({
-  type: TOGGLE_TODO,
-  id
-});
+export const toggleTodo = id => dispatch =>
+  api.toggleTodo(id).then(response => {
+    dispatch({
+      type: 'TOGGLE_TODO_SUCCESS',
+      response: normalize(response, schema.todo)
+    });
+  });
 
 // when fetchTodos action creator gets dispatched, it will call api, return a promise instead of a normal object
 // after the promise is resolved, dispatch that resolved action object
